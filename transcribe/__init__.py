21cf06aeed881263656fd9e9afa9918f918016e0
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-12-03 19:47:26 +0000

    Make this much better

--- transcribe/__init__.py
+++ transcribe/__init__.py
@@ -1,134 +1,174 @@
-# Bing subscriptions: https://www.microsoft.com/cognitive-services/en-us/subscriptions
-# Du brauchst "Bing speech"
-#
-# DO NOT RUN ON CLOUD DRIVE
-#
+# Best not to run this in a directory where other software tries to sync stuff
+# (dropbox, google drive, onedrive, etc.)
 
-from os import path
+from __future__ import absolute_import, division, print_function
+
+from optparse import SUPPRESS_HELP, OptionParser
+import os
 import speech_recognition as sr
 import time
 import wave
 
+def transcribe(filename, basename, mechs=None):
+  if not mechs:
+    print("No transcription mechanisms specified, skipping")
+    return
+  if not os.path.exists(filename):
+    print("Audio input file does not exist, skipping")
+    return
+
+  w = None
+  try:
+    w = wave.open(filename, 'r')
+    start_frame = 0
+    length = w.getnframes()
+    block_size = w.getframerate() * 11 # seconds
+    overlap = w.getframerate()    *  1 # seconds
+    lastframeend = start_frame
+
+    for m in mechs:
+      m.start(basename)
+
+    while lastframeend < w.getnframes()-1:
+      start = max(0, lastframeend - overlap)
+      end = min(lastframeend + block_size, w.getnframes()-1)
+      lastframeend = end + 1
+      print("Processing {start} to {end}".format(start=start, end=end))
+      w.setpos(start) # Set position on the original wav file
+      chunkData = w.readframes(end-start) # And read to where we need
+
+      # Create temporary file containing snippet of audio for processing  
+      chunkAudio = None
+      tempfile = 'temp.wav'
+      try: 
+        chunkAudio = wave.open(tempfile,'w')
+        chunkAudio.setnchannels(w.getnchannels())
+        chunkAudio.setsampwidth(w.getsampwidth())
+        chunkAudio.setframerate(w.getframerate())
+        chunkAudio.writeframes(chunkData)
+      finally:
+        if chunkAudio:
+          chunkAudio.close()
+
+      timecode = int(start / w.getframerate())
+      timecode = "[%02d:%02d] " % (int(timecode / 60), timecode % 60)
+
+      for m in mechs:
+        m.recognize(tempfile, timecode)
+  finally:
+    if w:
+      w.close()
+    for m in mechs:
+      m.done()
+  print("All done.")
+
 def main():
-  filename = path.join(path.dirname(path.realpath(__file__)), "output.wav")
-  language = "de-DE"
+  parser = OptionParser(usage="transcribe [options] file [file ..]",
+                        description="Transcribe one or more files to text using different methods.")
+  parser.add_option("-?", action="help", help=SUPPRESS_HELP)
+  parser.add_option("-b", "--bing", dest="bing", metavar="APIKEY",
+      action="store", type="string", default=None,
+      help="Bing API key. Register at https://www.microsoft.com/cognitive-services/en-us/subscriptions for 'Bing speech'.")
+  parser.add_option("-l", "--language", dest="language", metavar="LANG",
+      action="store", type="choice", default="en", choices=['en', 'de'],
+      help="Language of audio file (supported: en=english (default), de=german)")
+  options, args = parser.parse_args()
 
-  if language == 'en-US':
-    with open("output_sphinx.txt", "a") as fh:
-      fh.write("\n\nSphinx text recognition\n")
-  with open("output_bing.txt", "a") as fh:
-    fh.write("\n\nBing text recognition\n")
+  mechs = []
 
-  w = wave.open(filename, 'r')
-  start_frame = 0
-  length = w.getnframes()
-  block_size = w.getframerate() * 11 # Sekunden
-  overlap = w.getframerate()    *  1 # Sekunden
+  if options.language == 'en':
+    mechs.append(('sphinx', transcribe_sphinx(options)))
+    options.language = 'en-US'
+  elif options.language == 'de':
+    options.language = 'de-DE'
+  if options.bing:
+    mechs.append(('bing', transcribe_bing(options)))
 
-  lastframeend = start_frame
-  rate_limit = 0
-  while lastframeend < w.getnframes()-1:
-    start = max(0, lastframeend - overlap)
-    end = min(lastframeend + block_size, w.getnframes()-1)
-    lastframeend = end + 1
-    print (str(start) + ' to ' + str(end))
-    w.setpos(start) # Set position on the original wav file
-    chunkData = w.readframes(end-start) # And read to where we need
-   
-    chunkAudio = wave.open("temp.wav",'w')
-    chunkAudio.setnchannels(w.getnchannels())
-    chunkAudio.setsampwidth(w.getsampwidth())
-    chunkAudio.setframerate(w.getframerate())
-    chunkAudio.writeframes(chunkData)
-    chunkAudio.close()
+  for f in args:
+    print("Transcribing file {filename} with language {language} using mechs {mechs}".format(filename=f, language=options.language, mechs=", ".join(m[0] for m in mechs)))
+    basename = os.path.splitext(f)[0]
+    transcribe(f, base=basename, mechs=mechs,
+               language=options.language)
 
-    timecode = int(start / w.getframerate())
-    timecode = "[%02d:%02d] " % (int(timecode / 60), timecode % 60)
+class transcribe_sphinx():
+  def __init__(self, options):
+    self.recognizer = sr.Recognizer()
+    self.language = options.language
 
-    print(timecode)
+  def start(self, basename):
+    self.done()
+    outname = basename + '_sphinx.txt'
+    self.output_file = open(outname, 'w')
+    print("Writing sphinx transcription to {}".format(outname))
 
-    # use the audio file as the audio source
-    r = sr.Recognizer()
-    print("Loading...")
-    with sr.AudioFile("temp.wav") as source:
-      audio = r.record(source) # read the entire audio file
-    print("Recognizing...")
-  
+  def recognize(self, tempfile, timecode):
+    with sr.AudioFile(tempfile) as source:
+      audio = self.recognizer.record(source) # read the snippet file
     # recognize speech using Sphinx
-    if language == 'en-US':
-      try:
-        recognized = r.recognize_sphinx(audio, language=language)
-        with open("output_sphinx.txt", "a") as fh:     
-          fh.write("\n" + timecode)
-          fh.write(recognized.encode("UTF-8") + "\n")
-        print("Sphinx thinks you said:")
-        print(recognized)
-      except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-      except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
-  
-  # recognize speech using Google Speech Recognition
-  #try:
-  #    # for testing purposes, we're just using the default API key
-  #    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-  #    # instead of `r.recognize_google(audio)`
-  #    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-  #except sr.UnknownValueError:
-  #    print("Google Speech Recognition could not understand audio")
-  #except sr.RequestError as e:
-  #    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-  
-  # recognize speech using Wit.ai
-  #WIT_AI_KEY = "INSERT WIT.AI API KEY HERE" # Wit.ai keys are 32-character uppercase alphanumeric strings
-  #try:
-  #    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-  #except sr.UnknownValueError:
-  #    print("Wit.ai could not understand audio")
-  #except sr.RequestError as e:
-  #    print("Could not request results from Wit.ai service; {0}".format(e))
-  
-  # recognize speech using Microsoft Bing Voice Recognition
-    BING_KEY = "asdf"
     try:
-      if time.time() < rate_limit + 3.1:
-        print ("Waiting...")
-        time.sleep(3.1 + rate_limit - time.time())
-        print ("Go.")
-      rate_limit = time.time()
-      recognized = r.recognize_bing(audio, language=language, key=BING_KEY, show_all=True)
-      with open("output_bing.txt", "a") as fh:
-          for entry in recognized.get('results', []):
-  #        fh.write("\n" + timecode + "parsed: " + entry.get('name').encode("UTF-8") + "\n")
-              fh.write(timecode + entry.get('lexical').encode("UTF-8") + "\n")
-  #        fh.write("test" + results + "\n")
-          print("Microsoft Bing Voice Recognition:")
-          try:
-            print(entry.get('name'))
-          except:
-            pass
+      recognized = self.recognizer.recognize_sphinx(audio, language=self.language)
+      self.output_file.write("\n" + timecode)
+      self.output_file.write(recognized.encode("UTF-8") + "\n")
+      if recognized:
+        print("Sphinx thinks you said:", recognized)
+    except sr.UnknownValueError:
+      print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+      print("Sphinx error; {0}".format(e))
+
+  def done(self):
+    if self.output_file:
+      self.output_file.close()
+      self.output_file = None
+
+class transcribe_bing():
+  def __init__(self, options):
+    self.recognizer = sr.Recognizer()
+    self.language = options.language
+    self.api_key = options.bing
+    self.last_request = 0
+    self.rate_limit = 3.1 # minimum number of seconds between requests
+
+  def start(self, basename):
+    self.done()
+    outname = basename + '_bing.txt'
+    debname = basename + '_bing_debug.txt'
+    self.output_file = open(outname, 'w')
+    self.debug_file = open(debname, 'w')
+    print("Writing bing transcription to {}".format(outname))
+    print("Writing bing debug information to {}".format(debname))
+
+  def recognize(self, tempfile, timecode):
+    with sr.AudioFile(tempfile) as source:
+      audio = self.recognizer.record(source) # read the snippet file
+    # recognize speech using Bing
+    wait_time = self.last_request + self.rate_limit - time.time()
+    if wait_time > 0:
+      time.sleep(wait_time)
+    self.last_request = time.time()
+    try:
+      recognized = self.recognizer.recognize_bing(audio, language=self.language, key=self.api_key, show_all=True)
+      self.debug_file.write("\n" + timecode)
+      self.debug_file.write(recognized)
+      from pprint import pprint
+      pprint(recognized)
+      for entry in recognized.get('results', []):
+        self.output_file.write(timecode + entry.get('lexical').encode("UTF-8") + "\n")
+      try:
+        print("Bing thinks you said:", end="")
+        print(entry.get('name'))
+      except:
+        pass
     except sr.UnknownValueError:
       print("Microsoft Bing Voice Recognition could not understand audio")
     except sr.RequestError as e:
       print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-  
-  # recognize speech using Houndify
-  #HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE" # Houndify client IDs are Base64-encoded strings
-  #HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE" # Houndify client keys are Base64-encoded strings
-  #try:
-  #    print("Houndify thinks you said " + r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-  #except sr.UnknownValueError:
-  #    print("Houndify could not understand audio")
-  #except sr.RequestError as e:
-  #    print("Could not request results from Houndify service; {0}".format(e))
-  
-  # recognize speech using IBM Speech to Text
-  #IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE" # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-  #IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE" # IBM Speech to Text passwords are mixed-case alphanumeric strings
-  #try:
-  #    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-  #except sr.UnknownValueError:
-  #    print("IBM Speech to Text could not understand audio")
-  #except sr.RequestError as e:
-  #    print("Could not request results from IBM Speech to Text service; {0}".format(e))
+      self.debug_file.write(e)
 
+  def done(self):
+    if self.output_file:
+      self.output_file.close()
+      self.output_file = None
+    if self.debug_file:
+      self.debug_file.close()
+      self.debug_file = None
